d6b3d84ba058807484348042f317cd8a98dbbdc5
Illusion137 <raygoza.d@hotmail.com>
2026-04-11 16:25:21 -0700

    feat: server modifications to support ejs layout n stuff + create that layout + header boilerplate

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config;
 const express = require('express');
+const express_layouts = require('express-ejs-layouts');
 const app = express();
 
 const port = process.env.PORT ?? 3000;
@@ -8,12 +9,14 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
+app.use(express_layouts);
 
 app.use('/', (req, res) => {
-	res.status(404).render('pages/not_found');
+	res.status(404).render('pages/not_found', { title: 'Catalog' });
 });
 app.use((req, res) => {
-	res.status(404).render('pages/not_found');
+	res.status(404).render('pages/not_found', { title: 'Not Found' });
 });
 
 app.listen(port, () => {
