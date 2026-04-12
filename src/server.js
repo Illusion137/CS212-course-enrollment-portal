2681842cd1bcef66dbdb095d0cce108b7319ed16
Illusion137 <raygoza.d@hotmail.com>
2026-04-11 17:12:01 -0700

    fix: better ejs support and doinkling around with stuff like that

--- src/server.js
+++ src/server.js
@@ -10,10 +10,18 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main');
+app.set('layout extractScripts', true);
 app.use(express_layouts);
 
+const search_filters = {
+	CS: {
+		long_title: 'CS - Computer Science',
+		course_numbers: ['136', '136L', '105', '205', '305', '249'],
+	},
+};
+
 app.use('/', (req, res) => {
-	res.status(404).render('pages/not_found', { title: 'Catalog' });
+	res.status(404).render('pages/catalog', { title: 'Catalog', search_filters });
 });
 app.use((req, res) => {
 	res.status(404).render('pages/not_found', { title: 'Not Found' });
