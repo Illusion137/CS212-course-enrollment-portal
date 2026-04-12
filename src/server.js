be867dbe56dbec859f495750f4720618fcb731d1
Illusion137 <raygoza.d@hotmail.com>
2026-04-11 18:16:40 -0700

    fix: I lowkey forgot to use ".get" instead of ".use" for the frontend routes n stuff

--- src/server.js
+++ src/server.js
@@ -23,29 +23,36 @@ const search_filters = {
 // FIXME Yall do this however yall want, just a temp placeholder :3
 const all_courses = {
 	'CS 136': {
+		subject: 'CS',
+		course_nbr: '136',
 		title: 'C Programming',
+	},
+};
+const all_course_sections = {
+	36780: {
+		course: all_courses['CS 136'],
 	},
 };
 
 // FIXME Same for here, these are just here so I can test, but yall do whatever yall want with this as long as it works
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.render('pages/catalog', { title: 'Catalog', search_filters });
 });
-app.use('/course/:course_id', (req, res) => {
-	const { course_id } = req.params;
-	const course = all_courses[course_id];
-	res.render('pages/catalog', { title: 'Catalog', course });
+app.get('/course/:section_id', (req, res) => {
+	const { section_id } = req.params;
+	const section = all_course_sections[section_id];
+	res.render('pages/course_details', { title: section.course.title, section });
 });
-app.use('/view-courses', (req, res) => {
+app.get('/view-courses', (req, res) => {
 	res.render('pages/view_courses', { title: 'View Courses' });
 });
-app.use('/manage-courses', (req, res) => {
+app.get('/manage-courses', (req, res) => {
 	res.render('pages/manage_courses', { title: 'Manage Courses' });
 });
-app.use('/notifications', (req, res) => {
+app.get('/notifications', (req, res) => {
 	res.render('pages/notifications', { title: 'Notifications' });
 });
-app.use('/override', (req, res) => {
+app.get('/override', (req, res) => {
 	res.render('pages/override', { title: 'Course Override' });
 });
 app.use((req, res) => {
