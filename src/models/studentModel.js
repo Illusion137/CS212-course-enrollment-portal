a99ca9722083454fb08f864274a8b13cc9c1ff32
dki4-dev <dki4@nau.edu>
2026-04-25 09:43:08 -0700

    Update studentModel.js

--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -15,9 +15,34 @@ function getStudentById(id) {
 function saveStudents(students) {
 	writeDB(STUDENTS_DB, students);
 }
+// get all notifications for a student
+function getNotifications(id) {
+	const student = getStudentById(id);
+	
+	if (!student) {
+		throw new Error('Student not found');
+	}
 
+		return student.notifications || [];
+
+}
+
+// Get unread notification count 
+function getUnreadNotificxationCount(id) {
+	const student = getStudentByID(id);
+
+	if (!student) {
+		throw new error('Student not found');
+	}
+
+	const notifications = student.notifications || [];
+
+	return notifications.filter(n=> !n.read).length
+}
 module.exports = {
 	getAllStudents,
 	getStudentById,
-	saveStudents
+	saveStudents,
+	getNotifications,
+	getUnreadNotificationCount
 };
