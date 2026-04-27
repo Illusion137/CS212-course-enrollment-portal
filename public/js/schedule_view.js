ef005db57a61f2f41e4d98d3fbfd6e2067e7c9b3
Illusion137 <raygoza.d@hotmail.com>
2026-04-26 18:15:05 -0700

    feat: schedule view, show conflicts

--- public/js/schedule_view.js
+++ public/js/schedule_view.js
@@ -31,7 +31,7 @@ function render_day(day, course, color, top_percent, height_percent) {
 		$('<div>').addClass('schedule-block-label').text(course.label),
 		$('<div>')
 			.addClass('schedule-block-time')
-			.text(course.startTime + ' - ' + course.endTime)
+			.text(course.startTime + ' - ' + course.endTime),
 	);
 	if (course.room) {
 		time_block.append($('<div>').addClass('schedule-block-time').text(course.room));
@@ -53,9 +53,51 @@ function render_course(i, course) {
 	}
 }
 
+function find_conflicts(courses) {
+	const conflicts = [];
+	for (let i = 0; i < courses.length; i++) {
+		for (let j = i + 1; j < courses.length; j++) {
+			const course_a = courses[i];
+			const course_b = courses[j];
+			const course_a_days = new Set(parse_days(course_a.days));
+			const course_b_days = new Set(parse_days(course_b.days));
+			const shared_days = course_a_days.intersection(course_b_days);
+			if (shared_days.size === 0) continue;
+			const course_a_start = parse_time(course_a.startTime);
+			const course_a_end = parse_time(course_a.endTime);
+			const course_b_start = parse_time(course_b.startTime);
+			const course_b_end = parse_time(course_b.endTime);
+			if (course_a_start < course_b_end && course_b_start < course_a_end) {
+				conflicts.push({ course_a, course_b });
+			}
+		}
+	}
+	return conflicts;
+}
+
+function render_conflicts(courses) {
+	const banner = $('#schedule-conflict-banner');
+	const body = $('#schedule-conflict-body');
+	const conflicts = find_conflicts(courses);
+	if (conflicts.length === 0) {
+		banner.hide();
+		return;
+	}
+	body.empty();
+	for (const { course_a, course_b } of conflicts) {
+		const course_a_text = `${course_a.label} (${course_a.days} ${course_a.startTime}-${course_a.endTime})`;
+		const course_b_text = `${course_b.label} (${course_b.days} ${course_b.startTime}-${course_b.endTime})`;
+		const inner_html = `Conflict between Courses: <br/>&emsp;${course_a_text} and <br/>&emsp;${course_b_text}`;
+
+		body.append($('<p>').addClass('schedule-conflict-item').html(inner_html));
+	}
+	banner.show();
+}
+
 function render_schedule(courses) {
 	$('.schedule-day-body .schedule-block').remove();
 	for (const [i, course] of Object.entries(courses)) {
 		render_course(i, course);
 	}
+	render_conflicts(courses);
 }
