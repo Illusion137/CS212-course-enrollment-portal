fa59df46895260c490da2cc793241f9c6056ac76
Roskita <roska7134@gmail.com>
2026-04-27 13:12:21 -0700

    db gone

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,48 @@
-const fs = require('fs');
 
+const fs = require('fs');
+ 
 function readDB(path) {
 	return JSON.parse(fs.readFileSync(path));
 }
-
-function writeDB(path, data) {
-	fs.writeFileSync(path, JSON.stringify(data, null, 2));
+ 
+// initializes a queue for each write request 
+const writeQueues = new Map();
+ 
+function flushQueue(path) {
+	const queue = writeQueues.get(path);
+	if (!queue || queue.length === 0) return;
+ 
+	const { data, resolve, reject } = queue[0];
+ 
+	try {
+		fs.writeFileSync(path, JSON.stringify(data, null, 2));
+		resolve();
+	} catch (err) {
+		reject(err);
+	} finally {
+		queue.shift();
+		if (queue.length > 0) {
+			setImmediate(() => flushQueue(path));
+		}
+	}
 }
 
+// returns a promise that resolves once write is done
+function writeDB(path, data) {
+	return new Promise((resolve, reject) => {
+		if (!writeQueues.has(path)) {
+			writeQueues.set(path, []);
+		}
+ 
+		const queue = writeQueues.get(path);
+		const wasIdle = queue.length === 0;
+ 
+		queue.push({ data, resolve, reject });
+ 
+		if (wasIdle) {
+			flushQueue(path);
+		}
+	});
+}
+ 
 module.exports = { readDB, writeDB };
